4be45700b447af7189b7a92f671c136ce177c4fd
abderrahmaneTahar <80069532+abderrahmaneTahar@users.noreply.github.com>
2021-09-13 11:11:38 +0100

    change required python version

--- setup.py
+++ setup.py
@@ -31,8 +31,9 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        
     ]
 )
